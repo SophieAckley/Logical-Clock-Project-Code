a89350a96d69b891c48d35cbee8c91ea9f2d6258
QIYI YIN <111394249+SophieAckley@users.noreply.github.com>
2024-11-17 02:24:25 -0800

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,62 +1,102 @@
-import grpc
-import banks_pb2
-import banks_pb2_grpc
-import time
-
-class Customer:
-    def __init__(self, id, requests):
-        self.id = id
-        self.requests = requests
-        self.logical_clock = 0
-        self.events = []
-    
-    def execute_requests(self, stub):
-        try:
-            for request in self.requests:
-                self.logical_clock += 1
-            request_id = request['customer-request-id']
-            interface = request['interface']
-            money = request.get('money', 0)
-            
-            print(f"Customer {self.id} executing {interface} request {request_id}")
-            
-            # 执行请求
-            if interface == 'query':
-                response = stub.Query(banks_pb2.QueryRequest(
-                    customer_id=self.id,
-                    request_id=request_id,
-                    logical_clock=self.logical_clock
-                ))
-            elif interface == 'deposit':
-                response = stub.Deposit(banks_pb2.DepositRequest(
-                    customer_id=self.id,
-                    request_id=request_id,
-                    amount=money,
-                    logical_clock=self.logical_clock
-                ))
-            elif interface == 'withdraw':
-                response = stub.Withdraw(banks_pb2.WithdrawRequest(
-                    customer_id=self.id,
-                    request_id=request_id,
-                    amount=money,
-                    logical_clock=self.logical_clock
-                ))
-            
-            # 更新逻辑时钟
-            self.logical_clock = max(self.logical_clock, response.logical_clock) + 1
-            
-            # 记录事件
-            event = {
-                "customer-request-id": request_id,
-                "logical_clock": self.logical_clock,
-                "interface": interface,
-                "comment": f"event_sent from customer {self.id}"
-            }
-            print(f"Adding event: {event}")
-            self.events.append(event)
-            
-            print(f"Request {request_id} completed, logical_clock: {self.logical_clock}")
-            
-        except Exception as e:
-            print(f"Error executing requests for customer {self.id}: {str(e)}")
-            raise
+[
+  {
+    "id": 1,
+    "type": "customer",
+    "events": [
+      {
+        "customer-request-id": 2,
+        "logical_clock": 10,
+        "interface": "withdraw",
+        "comment": "event_sent from customer 1"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "type": "customer",
+    "events": [
+      {
+        "customer-request-id": 4,
+        "logical_clock": 17,
+        "interface": "withdraw",
+        "comment": "event_sent from customer 2"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "type": "customer",
+    "events": [
+      {
+        "customer-request-id": 6,
+        "logical_clock": 24,
+        "interface": "withdraw",
+        "comment": "event_sent from customer 3"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "type": "customer",
+    "events": [
+      {
+        "customer-request-id": 8,
+        "logical_clock": 31,
+        "interface": "withdraw",
+        "comment": "event_sent from customer 4"
+      }
+    ]
+  },
+  {
+    "id": 1,
+    "type": "branch",
+    "events": []
+  },
+  {
+    "id": 2,
+    "type": "branch",
+    "events": []
+  },
+  {
+    "id": 3,
+    "type": "branch",
+    "events": []
+  },
+  {
+    "id": 4,
+    "type": "branch",
+    "events": []
+  },
+  {
+    "id": 1,
+    "customer-request-id": 2,
+    "type": "customer",
+    "logical_clock": 10,
+    "interface": "withdraw",
+    "comment": "event_sent from customer 1"
+  },
+  {
+    "id": 2,
+    "customer-request-id": 4,
+    "type": "customer",
+    "logical_clock": 17,
+    "interface": "withdraw",
+    "comment": "event_sent from customer 2"
+  },
+  {
+    "id": 3,
+    "customer-request-id": 6,
+    "type": "customer",
+    "logical_clock": 24,
+    "interface": "withdraw",
+    "comment": "event_sent from customer 3"
+  },
+  {
+    "id": 4,
+    "customer-request-id": 8,
+    "type": "customer",
+    "logical_clock": 31,
+    "interface": "withdraw",
+    "comment": "event_sent from customer 4"
+  }
+]
