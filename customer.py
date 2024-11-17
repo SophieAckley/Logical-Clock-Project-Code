018a9f66cd670debd6cb51584d2a2284b250d22a
QIYI YIN <111394249+SophieAckley@users.noreply.github.com>
2024-11-16 22:47:30 -0800

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,34 +1,50 @@
 import grpc
 import banks_pb2
 import banks_pb2_grpc
-import time
 
 class Customer:
-    def __init__(self, id, events):
+    def __init__(self, id, requests):
         self.id = id
-        self.events = events
-        self.recvMsg = []
-        self.stub = None
-
-    def createStub(self):
-        channel = grpc.insecure_channel(f'localhost:{50000 + self.id}')
-        self.stub = banks_pb2_grpc.BankServiceStub(channel)
-
-    def executeEvents(self):
-        if not self.stub:
-            self.createStub()
-
-        logical_clock = 0
-        for event in self.events:
-            logical_clock += 1  # Increment clock for each event
-            request = banks_pb2.Request(
-                id=event['id'],
-                interface=event['interface'],
-                money=event.get('money', 0),
-                logical_clock=logical_clock
-            )
-            response = self.stub.MsgDelivery(request)
-            logical_clock = max(logical_clock, response.logical_clock) + 1
-            self.recvMsg.append(response)
-            time.sleep(0.5)
-        return self.recvMsg
+        self.requests = requests
+        self.logical_clock = 0
+        self.events = []
+    
+    def execute_requests(self):
+        with grpc.insecure_channel('localhost:50051') as channel:
+            stub = banks_pb2_grpc.BankStub(channel)
+            
+            for request in self.requests:
+                self.logical_clock += 1
+                request_id = request['customer-request-id']
+                interface = request['interface']
+                money = request['money']
+                
+                if interface == 'query':
+                    response = stub.Query(banks_pb2.QueryRequest(
+                        customer_id=self.id,
+                        request_id=request_id,
+                        logical_clock=self.logical_clock
+                    ))
+                elif interface == 'deposit':
+                    response = stub.Deposit(banks_pb2.DepositRequest(
+                        customer_id=self.id,
+                        request_id=request_id,
+                        amount=money,
+                        logical_clock=self.logical_clock
+                    ))
+                elif interface == 'withdraw':
+                    response = stub.Withdraw(banks_pb2.WithdrawRequest(
+                        customer_id=self.id,
+                        request_id=request_id,
+                        amount=money,
+                        logical_clock=self.logical_clock
+                    ))
+                
+                self.logical_clock = max(self.logical_clock, response.logical_clock) + 1
+                
+                self.events.append({
+                    "customer-request-id": request_id,
+                    "logical_clock": self.logical_clock,
+                    "interface": interface,
+                    "comment": f"event_sent from customer {self.id}"
+                })
